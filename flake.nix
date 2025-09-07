2aead01cf5658f2e6ba7e21d142baa75724b0225
John Morris <2e0byo@gmail.com>
2025-09-07 13:53:52 +0200

    refactor: no flake-utils.

--- flake.nix
+++ flake.nix
@@ -3,41 +3,42 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    flake-utils.url = "github:numtide/flake-utils";
     deploy-rs.url = "github:serokell/deploy-rs";
   };
 
-  outputs = inputs: 
-   with inputs;
-      flake-utils.lib.eachDefaultSystem (system: let
-        pkgs = nixpkgs.legacyPackages."${system}";
-        crossPkgs = import "${nixpkgs}" { 
-          localSystem = system;
-          crossSystem = "aarch64-linux";
-        };
-    in rec {
-      nixosConfigurations = {
-        zero2w = nixpkgs.lib.nixosSystem {
-          modules = [
-            "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
-            ./zero2w.nix
-            {
-              nixpkgs.pkgs = crossPkgs; # configure cross compilation. If the build system `system` is aarch64, this will provide the aarch64 nixpkgs
-            }
-          ];
-        };
+  outputs = {
+    self,
+    nixpkgs,
+    deploy-rs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages."${system}";
+    crossPkgs = import "${nixpkgs}" {
+      localSystem = system;
+      crossSystem = "aarch64-linux";
+    };
+  in rec {
+    nixosConfigurations = {
+      zero2w = nixpkgs.lib.nixosSystem {
+        modules = [
+          "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+          ./zero2w.nix
+          {
+            nixpkgs.pkgs = crossPkgs; # configure cross compilation. If the build system `system` is aarch64, this will provide the aarch64 nixpkgs
+          }
+        ];
       };
+    };
 
-      deploy = {
-        user = "root";
-        nodes = {
-          zero2w = {
-            hostname = "zero2w";
-            profiles.system.path =
-              deploy-rs.lib.aarch64-linux.activate.nixos self.nixosConfigurations.zero2w;
-          };
+    deploy = {
+      user = "root";
+      nodes = {
+        zero2w = {
+          hostname = "zero2w";
+          profiles.system.path =
+            deploy-rs.lib.aarch64-linux.activate.nixos self.nixosConfigurations.zero2w;
         };
       };
-    }
-  );
+    };
+  };
 }
