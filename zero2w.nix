62668156a2a68d656e191c2c2daccda978d8244a
John Morris <2e0byo@gmail.com>
2025-11-01 17:04:25 +0100

    feat: setup.

--- zero2w.nix
+++ zero2w.nix
@@ -3,22 +3,63 @@
   modulesPath,
   pkgs,
   ...
-}:
-{
+}: {
   imports = [
     ./sd-image.nix
   ];
 
-  # Some packages (ahci fail... this bypasses that) https://discourse.nixos.org/t/does-pkgs-linuxpackages-rpi3-build-all-required-kernel-modules/42509
   nixpkgs.overlays = [
+    # Some packages (ahci fail... this bypasses that) https://discourse.nixos.org/t/does-pkgs-linuxpackages-rpi3-build-all-required-kernel-modules/42509
     (final: super: {
-      makeModulesClosure = x: super.makeModulesClosure (x // { allowMissing = true; });
+      makeModulesClosure = x: super.makeModulesClosure (x // {allowMissing = true;});
     })
+
+    (final: super: {
+      python3 = super.python3.override {
+        packageOverrides = python-self: python-super: {
+          gst-python = python-super.gst-python.overrideAttrs (old: {
+            mesonFlags =
+              (old.mesonFlags or [])
+              ++ [
+                # Don't even try to build the testsuite
+                "-Dtests=disabled"
+              ];
+            doCheck = false;
+          });
+        };
+      };
+    })
+
+    (final: prev: {
+      python3Packages = prev.python3Packages.overrideScope (python-final: python-prev: {
+        tidalapi = python-prev.tidalapi.overrideAttrs (attrs: rec {
+          version = "0.8.6";
+          src = prev.fetchFromGitHub {
+            owner = "EbbLabs";
+            repo = "python-tidal";
+            tag = "v${version}";
+            hash = "sha256-SsyO0bh2ayHfGzINBW1BTTPS/ICvIymIhQ1HUPRFOwU=";
+          };
+        });
+      });
+      mopidy-tidal = prev.mopidy-tidal.overrideAttrs (_: rec {
+        pname = "latest-mopidy-tidal";
+        version = "0.3.11";
+        src = prev.fetchFromGitHub {
+          owner = "EbbLabs";
+          repo = "mopidy-tidal";
+          rev = "v${version}";
+          hash = "sha256-wqx/30UQVm1fEwP/bZeW7TtzGfn/wI0klQnFr9E3AOs=";
+        };
+      });
+    })
+
   ];
 
   nixpkgs.hostPlatform = "aarch64-linux";
   # ! Need a trusted user for deploy-rs.
-  nix.settings.trusted-users = [ "@wheel" ];
+  nix.settings.trusted-users = ["@wheel"];
+  nix.settings.experimental-features = ["nix-command" "flakes"];
   system.stateVersion = "24.05";
 
   zramSwap = {
@@ -26,10 +67,14 @@
     algorithm = "zstd";
   };
 
+  # TODO does the rest need to move into this namespace?
+  image = {
+    fileName = "zero2.img";
+  };
   sdImage = {
     # bzip2 compression takes loads of time with emulation, skip it. Enable this if you're low on space.
     compressImage = false;
-    imageName = "zero2.img";
+    # imageName = "zero2.img";
 
     extraFirmwareConfig = {
       # Give up VRAM for more Free System Memory
@@ -47,7 +92,7 @@
 
   hardware = {
     enableRedistributableFirmware = lib.mkForce false;
-    firmware = [ pkgs.raspberrypiWirelessFirmware ]; # Keep this to make sure wifi works
+    firmware = [pkgs.raspberrypiWirelessFirmware]; # Keep this to make sure wifi works
     i2c.enable = true;
 
     deviceTree = {
@@ -90,11 +135,11 @@
     interfaces."wlan0".useDHCP = true;
     wireless = {
       enable = true;
-      interfaces = [ "wlan0" ];
+      interfaces = ["wlan0"];
       # ! Change the following to connect to your own network
       networks = {
-        "<ssid>" = {
-          psk = "<ssid-key>";
+        "Livebox-6300" = {
+          psk = "tAyZq95Rn4L5eGEdvZ";
         };
       };
     };
@@ -107,16 +152,21 @@
   services.timesyncd.enable = true;
 
   # ! Change the following configuration
-  users.users.bob = {
+  users.mutableUsers = false;
+  users.users.mopidy.extraGroups = [ "pipewire" ];
+  users.users.john = {
     isNormalUser = true;
-    home = "/home/bob";
-    description = "Bob";
+    home = "/home/john";
+    description = "John";
     extraGroups = [
       "wheel"
       "networkmanager"
     ];
+    hashedPassword = "$y$j9T$eKhMANNrwREak4/HLP77I/$8Uz7HqZXKVnGoM1YRLIaDJUdNygA6cWv8C/bZSDf6ND";
     # ! Be sure to put your own public key here
-    openssh.authorizedKeys.keys = [ "a public key" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGPxIqAr93XSYcZMA+AMpH8txaQjCHS03Trb+6KAB8tJ john@nixos"
+    ];
   };
 
   security.sudo = {
@@ -124,10 +174,10 @@
     wheelNeedsPassword = false;
   };
   # ! Be sure to change the autologinUser.
-  services.getty.autologinUser = "bob";
+  services.getty.autologinUser = "john";
 
   # ! change the host name if you like
-  networking.hostName = "pi";
+  networking.hostName = "reepicheep";
 
   services.avahi = {
     enable = true;
@@ -140,4 +190,54 @@
       workstation = true;
     };
   };
+
+  environment.systemPackages = [
+    pkgs.btop
+    pkgs.vim
+    pkgs.tmux
+    pkgs.alsa-utils
+    pkgs.ncpamixer
+    pkgs.git
+    # pkgs.git-annex
+  ];
+
+  services.mopidy = {
+    enable = true;
+    extensionPackages = with pkgs; [
+      mopidy-local
+      mopidy-iris
+      mopidy-mpd
+      mopidy-mpris
+      mopidy-tidal
+    ];
+    settings = {
+      local.media_dir = "/var/lib/mopidy/Library";
+      m3u = {
+        playlists_dir = "/var/lib/mopidy/playlists/playlists";
+        base_dir = "/var/lib/mopidy/Library";
+      };
+      tidal.quality = "LOSSLESS";
+      tidal.auth_method = "PKCE";
+      http.hostname = "0.0.0.0";
+    };
+  };
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    systemWide = true;
+  };
+
+  networking.firewall.enable = true;
+  # services.ssh opens it's own port
+  networking.firewall.allowedTCPPorts = [
+    6680 # mopidy
+    8989 # mopidy login
+  ];
+
+  programs.bash.shellInit = ''
+  set -o vi
+  '';
 }
